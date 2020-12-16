3cc137b2b9193919451824d2a4512564497bc4df
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-12-15 21:31:38 -0500

    Update ui.js

--- ui.js
+++ ui.js
@@ -6,8 +6,10 @@ $(document).on('click', '#hamburger' , function() {
     if ($(this).attr('aria-checked') === 'true') {
         $(this).attr('aria-checked', 'false');
         $('.tray').slideToggle();
+        $('body').css('overflow', 'scroll');
     } else {
         $(this).attr('aria-checked', 'true');
         $('.tray').slideToggle();
+        $('body').css('overflow', 'hidden');
     }
 });
